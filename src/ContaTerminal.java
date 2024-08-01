ed880965a5d978c08eda72788b2fd858c954bfb6
Xavier <xavier.tjrs@gmail.com>
2024-08-01 08:07:33 -0300

    primeiro commit

---
+++ src/ContaTerminal.java
@@ -0,0 +1,11 @@
+public class ContaTerminal {
+
+    int numero = 1021;
+    String agencia = "067-8";
+    String cliente = "MARIO ANDRADE";
+    double saldo = 237.48;
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
